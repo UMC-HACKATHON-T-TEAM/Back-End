56063481046ff045ac51248abb0071b174f8d09e
Jinho622 <aceho21@gmail.com>
2025-01-11 17:26:38 +0900

    feat: #1 init project

---
+++ src/main/java/UMC_Hackathon/DailyMate/DailyMateApplication.java
@@ -0,0 +1,13 @@
+package UMC_Hackathon.DailyMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/UMC_Hackathon/DailyMate/DailyMateApplicationTests.java
@@ -0,0 +1,13 @@
+package UMC_Hackathon.DailyMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
