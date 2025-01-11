29cff26108e24dc8fb7aa221dc016092bf8c4a38
벨라 <icomksh@gmail.com>
2025-01-12 02:53:09 +0900

    [Refactor] 명세서 설명 추가

--- src/main/java/UMC_Hackathon/DailyMate/web/controller/UserRestController.java
+++ src/main/java/UMC_Hackathon/DailyMate/web/controller/UserRestController.java
@@ -19,14 +19,14 @@ public class UserRestController {
     private final UserCommandService userCommandService;
 
     @PostMapping("/")
-    @Operation(summary = "사용자 회원가입 API")
+    @Operation(summary = "사용자 회원가입 API", description = "사용자 입력값으로 회원가입하는 API입니다.")
     public ApiResponse<UserResponseDTO.JoinResultDTO> join(@RequestBody @Valid UserRequestDTO.JoinDto request){
         Users users = userCommandService.joinUser(request);
         return ApiResponse.onSuccess(UserConverter.toJoinResultDTO(users));
     }
 
     @PostMapping("/login")
-    @Operation(summary = "사용자 로그인 API")
+    @Operation(summary = "사용자 로그인 API", description = "이메일과 비밀번호로 로그인하는 API입니다.")
     public ApiResponse<Users> login(@RequestParam String email, @RequestParam String password) {
         // 사용자 인증
         Users authenticatedUser = userCommandService.authenticateUser(email, password);
